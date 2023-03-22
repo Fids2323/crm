ee378d50c5d39fd408ec89d318f92314f27790b1
Dmitry Homskiy <workfids23@gmail.com>
2023-03-22 19:12:46 +0300

    add preview

--- script/index.js
+++ script/index.js
@@ -1,20 +1,23 @@
 import {
-	countTotalPrice,
-	deleteSupply,
-	addSupply,
-	openImage,
-} from './modules/control.js'
-import {getStorage} from './modules/serviceStorage.js'
-import modalControl from './modules/modalControl.js'
-import renderGoods from './modules/render.js'
+  countTotalPrice,
+  deleteSupply,
+  addSupply,
+  openImage,
+} from "./modules/control.js";
+import { getStorage } from "./modules/serviceStorage.js";
+import modalControl from "./modules/modalControl.js";
+import renderGoods from "./modules/render.js";
+import { createPreview } from "./modules/createElements.js";
+
 {
-	const init = () => {
-		renderGoods(getStorage('data'))
-		modalControl()
-		countTotalPrice()
-		addSupply()
-		deleteSupply()
-		openImage()
-	};
-	window.crmInit = init;
+  const init = () => {
+    renderGoods(getStorage("data"));
+    createPreview();
+    modalControl();
+    countTotalPrice();
+    addSupply();
+    deleteSupply();
+    openImage();
+  };
+  window.crmInit = init;
 }
--- script/modules/createElements.js
+++ script/modules/createElements.js
@@ -9,29 +9,55 @@ const createElem = (tag, classes, text, data) => {
 
 // Create tableRow
 const createRow = (obj, index) => {
-  const tr = createElem('tr');
-  const td1 = createElem('td', 'table__cell', `${index}`);
-  const tdName = createElem('td',
-    'table__cell table__cell_left table__cell_name', obj.title, obj.id);
-  const span = createElem('span', 'table__cell-id', `id: ${obj.id} `);
+  const tr = createElem("tr");
+  const td1 = createElem("td", "table__cell", `${index}`);
+  const tdName = createElem(
+    "td",
+    "table__cell table__cell_left table__cell_name",
+    obj.title,
+    obj.id
+  );
+  const span = createElem("span", "table__cell-id", `id: ${obj.id} `);
   tdName.prepend(span);
-  const tdLeft = createElem('td', 'table__cell table__cell_left', obj.category);
-  const tdNum = createElem('td', 'table__cell', obj.units);
-  const tdCount = createElem('td', 'table__cell', obj.count);
-  const tdPrice = createElem('td', 'table__cell', '₽ ' + obj.price);
-  const tdTotalPrice = createElem('td', 'table__cell', '₽ ' + (obj.price * obj.count) / 100 * (100 - obj.discount));
-  const tdBtnWrap = createElem('td', 'table__cell table__cell_btn-wrapper');
-  const buttonPic = createElem('button', 'table__btn table__btn_pic');
-	buttonPic.dataset.pic = 'https://look.com.ua/pic/201704/800x600/look.com.ua-204614.jpg'
-  const buttonEdit = createElem('button', 'table__btn table__btn_edit');
-  const buttonDel = createElem('button', 'table__btn table__btn_del');
+  const tdLeft = createElem("td", "table__cell table__cell_left", obj.category);
+  const tdNum = createElem("td", "table__cell", obj.units);
+  const tdCount = createElem("td", "table__cell", obj.count);
+  const tdPrice = createElem("td", "table__cell", "₽ " + obj.price);
+  const tdTotalPrice = createElem(
+    "td",
+    "table__cell",
+    "₽ " + ((obj.price * obj.count) / 100) * (100 - obj.discount)
+  );
+  const tdBtnWrap = createElem("td", "table__cell table__cell_btn-wrapper");
+  const buttonPic = createElem("button", "table__btn table__btn_pic");
+  buttonPic.dataset.pic =
+    "https://look.com.ua/pic/201704/800x600/look.com.ua-204614.jpg";
+  const buttonEdit = createElem("button", "table__btn table__btn_edit");
+  const buttonDel = createElem("button", "table__btn table__btn_del");
   tdBtnWrap.append(buttonPic, buttonEdit, buttonDel);
-  tr.append(td1, tdName, tdLeft, tdNum,
-    tdCount, tdPrice, tdTotalPrice, tdBtnWrap);
+  tr.append(
+    td1,
+    tdName,
+    tdLeft,
+    tdNum,
+    tdCount,
+    tdPrice,
+    tdTotalPrice,
+    tdBtnWrap
+  );
   return tr;
 };
 
-export {
-	createElem,
-	createRow
-}
+const createPreview = () => {
+  const fieldset = document.querySelector(".modal__fieldset");
+  const wrapper = document.createElement("div");
+  const img = document.createElement("img");
+
+  img.className = "preview";
+  wrapper.className = "wrapper";
+  wrapper.append(img);
+  fieldset.after(wrapper);
+  img.style.cssText = "margin: 0 auto 20px; display: block;";
+};
+
+export { createElem, createRow, createPreview };
